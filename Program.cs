79e721f3054654d8a5f1e523e2df1a644d321149
Joao Quiziri <quizirironchi@hotmail.com>
2024-01-18 18:35:05 -0300

    Commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace StopWatch;
 
@@ -6,10 +7,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!")
+        Menu();
     }
-    static void Start()
-    {
 
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("S = Segundo => 10s = 10 segundos");
+        Console.WriteLine("M = Minuto => 1m = 1 minuto");
+        Console.WriteLine("0 = Sair");
+        Console.WriteLine("Quanto tempo deseja contar ?");
+
+        string data = Console.ReadLine().ToLower();
+        char type = char.Parse(data.Substring(data.Length-1, 1));
+        int time = int.Parse(data.Substring(0, data.Length -1));
+        int multiplier = 1;
+
+        if(type == 'm')
+        {
+            multiplier = 60;
+        }
+    }
+
+    static void Start(int time)
+    {
+        int currentTime = 0;
+        while (currentTime != time)
+        {
+            Console.Clear();
+            currentTime++;
+            Console.WriteLine(currentTime);
+            Thread.Sleep(1000);
+        }
+        Console.Clear();
+        Console.WriteLine("StopWatch finalizado");
+        Thread.Sleep(2500);
     }
 }
